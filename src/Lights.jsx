25376508aea04b18d41d9c6a9ecf400f9559deb2
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2020-09-03 19:36:54 -0700

    Working on ColorPicker implemetation

--- src/Lights.jsx
+++ src/Lights.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { ChromePicker } from 'react-color';
+import { SketchPicker } from 'react-color';
+import { rgbToXY, xyToRGB } from './colorConverter.js';
 import Axios from 'axios';
 
 // Notes: bri: 1-254, ct: 500(warmest)-153(coldest), hue: 0-65535
 
 export default function Lights(props) {
     const [lights, setLights] = useState([]);
+    const [picker, setPicker] = useState(false);
 
     useEffect(() => {
         Axios.get(props.url + '/lights').then(res => {
@@ -21,15 +23,27 @@ export default function Lights(props) {
     let turnOff = (lightNum) => {
       Axios.put(props.url + `/lights/${lightNum}/state`, {"on": false})
     }
+
+    let colorToggle = (lightNum) => { // Show the color picker for the corresponding light
+      let rgbColor; // Needs to return {r: 255, g: 255, b: 255} for react-color
+      Axios.get(props.url + `/lights${lightNum}`).then(res => {
+        rgbColor = xyToRGB(res.data.state.xy[0], res.data.state.xy[1])
+      })
+    }
+
+    let updateColor = (lightNum, color) => { // For the onChangeComplete of the ColorPicker
+      let xyColor = rgbToXY(color[0], color[1], color[2]) // Needs to return [0.2345, 0.9876] for the Hue API
+      Axios.put(props.url + `/lights/${lightNum}/state`, {"xy": xyColor})
+    }
   
     let content = '';
     if (lights.length > 0) {
-      content = lights.map((light, id) => <div key={id}><h1>{light}</h1><button onClick={() => turnOn(light)}>On</button><button onClick={() => turnOff(light)}>Off</button></div>)
+      content = lights.map((light, id) => <div key={id}><h1>{light}</h1><button onClick={() => turnOn(light)}>On</button><button onClick={() => turnOff(light)}>Off</button><button onClick={() => colorToggle(light)}>Color</button></div>)
     }
     return (
         <div className="App">
             <h1>Lights Page</h1>
-            <ChromePicker color="#fff" onChangeComplete={console.log("color.hex")} />
+            <SketchPicker class="none" disableAlpha={true} onChangeComplete={(color) => console.log(color)} />
             {content}
         </div>
     )
--- src/colorConverter.js
+++ src/colorConverter.js
@@ -1,8 +1,8 @@
-const scale = (num, in_min, in_max, out_min, out_max) => {
+const scale = (num, in_min, in_max, out_min, out_max) => { // To Scale numbers from a range down to another range
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   }
 
-export function rgbToXY(r, g, b) {
+export function rgbToXY(r, g, b) { // Return Format: [0.2553, 0.9112]
     //RGB values set in between 0 and 1
     r = scale(r, 0, 255, 0, 1)
     g = scale(g, 0, 255, 0, 1)
@@ -16,28 +16,26 @@ export function rgbToXY(r, g, b) {
     let yy = red * 0.234327 + green * 0.743075 + blue * 0.022598;
     let zz = red * 0 + green * 0.053077 + blue * 1.035763
     //Calculate XY values from XYZ
-    x = xx / (xx + yy + zz);
-    y = yy / (xx + yy + zz);
+    let x = xx / (xx + yy + zz);
+    let y = yy / (xx + yy + zz);
     //Use the Y value as brightness
     return {x, y, yy}
 }
 
-export function xyToRGB(x, y, brightness) {
+export function xyToRGB(x, y, brightness) { // Return Format: {r: 255, g: 255, b: 255}
     // Convert
     let z = 1 - x - y
     let Y = brightness
     let X = (Y / y) * x;
     let Z = (Y / y) * z
     // Convert to RGB
-    let r = X  * 1.4628067 - Y * 0.1840623 - Z * 0.2743606;
-    let g = -X * 0.5217933 + Y * 1.4472381 + Z * 0.0677227;
-    let b = X  * 0.0349342 - Y * 0.0968930 + Z * 1.2884099;
+    let R = X  * 1.4628067 - Y * 0.1840623 - Z * 0.2743606;
+    let G = -X * 0.5217933 + Y * 1.4472381 + Z * 0.0677227;
+    let B = X  * 0.0349342 - Y * 0.0968930 + Z * 1.2884099;
     // Reverse gamma correction
-    r = r <= 0.0031308 ? 12.92 * r : (1.0 + 0.055) * Math.pow(r, (1.0 / 2.4)) - 0.055;
-
-    g = g <= 0.0031308 ? 12.92 * g : (1.0 + 0.055) * Math.pow(g, (1.0 / 2.4)) - 0.055;
-
-    b = b <= 0.0031308 ? 12.92 * b : (1.0 + 0.055) * Math.pow(b, (1.0 / 2.4)) - 0.055;
+    let r = R <= 0.0031308 ? 12.92 * R : (1.0 + 0.055) * Math.pow(R, (1.0 / 2.4)) - 0.055;
+    let g = G <= 0.0031308 ? 12.92 * G : (1.0 + 0.055) * Math.pow(G, (1.0 / 2.4)) - 0.055;
+    let b = B <= 0.0031308 ? 12.92 * B : (1.0 + 0.055) * Math.pow(B, (1.0 / 2.4)) - 0.055;
 
     return {r, g, b}
 }
