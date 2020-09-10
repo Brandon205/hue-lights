3010e4ef778a1a7918f37effbbf931a667a52868
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2020-09-10 14:24:19 -0700

    Small CSS Tweaks

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
     return (
         <div className="container">
-            <h1>Welcome to another Hue Web App</h1>
+            <h1>Welcome to Yet Another Hue Wep App YAHWA</h1>
         </div>
     )
 }
