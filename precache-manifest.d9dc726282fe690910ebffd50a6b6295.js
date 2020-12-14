b2cfe2296d0bb5d4104b8a5dafe2f40d7a0bbf59
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2020-12-14 13:20:47 -0800

    Updates

---
+++ precache-manifest.d9dc726282fe690910ebffd50a6b6295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4088c1606e9558679ea5e094c643fed",
+    "url": "/hue-lights/index.html"
+  },
+  {
+    "revision": "5b551e9a61716190aee6",
+    "url": "/hue-lights/static/css/main.8bd6fcd3.chunk.css"
+  },
+  {
+    "revision": "99c0499aa8f1821fee3b",
+    "url": "/hue-lights/static/js/2.6152e44d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hue-lights/static/js/2.6152e44d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b551e9a61716190aee6",
+    "url": "/hue-lights/static/js/main.3eff3002.chunk.js"
+  },
+  {
+    "revision": "d4b058643230bee0c065",
+    "url": "/hue-lights/static/js/runtime-main.1d61fbc0.js"
+  }
+]);
